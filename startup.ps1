2fe6ded45fa12beb0a9754634f98b6008dca5f26
Thomas Williams <tjcertified@gmail.com>
2026-06-14 11:42:08 -0500

    start script

---
+++ startup.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "[+] Checking for WSL:"
+
+wsl --status *>&1 > $null
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "  - WSL Installed"
+} else {
+    Write-Host "  - Installing WSL"
+    wsl --install
+}
