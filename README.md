b89a114089824c144bb79b889b5f40ea89cf9c4c
Thomas Williams <tjcertified@gmail.com>
2026-06-14 21:57:00 -0500

    somewhat working demo

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # csc842-tool2
-Script to automate setup of secure software supply chain
+This is a script to automate setup of secure software supply chain.
+
+
+## Problem
+Getting a toolchain setup to learn and test a fully secured software supply chain is difficult with so many tools and so many security postures to consider. There is SLSA and SBOMs, provenance and attestation, SAST/DAST/SCA, system monitoring and all of the dashboards that go along with it. A system like this needs good testing and good startup resources to allow a learner to try things out in a low-risk setting.
+
+### Existing Tools
+While there are a few tools online, there is very little in the way of localized setup and testing, especially on the Windows OS.
+
+### Why Care?
+If a security practitioner cannot easily test and de-risk answers, then implementation will be far less likely with fewer implementers who really understand the whole lifecycle.
+
+### Intent
+This tool intended to setup both a secure starting environment for a kubernetes-driven local platform that was provably secure via attestations and secure initial containers for GitLab, Artifactory, and Chainguard with additional tutorials to walk through secure setup of pipelines with hello-world type repos for one or two common languages like python and C.
+
+However, getting the initial attestations implemented proved harder than expected. The resulting product simply installs tools and a Kubernetes environment where helm charts can be pulled in on a Windows 11 OS.
+
+## Design
+The design of this "product" was to be a single script to setup the environment in Windows and accompanying tutorials to walkthrough a few open-source tools to accomplish SBOM verification and product attestation within this provably secure environment.
+
+This iteration only implements the initial environment of Kubernetes with a simple SigStore that I could not get to work in a localized environment. I have not worked to the root of this problem yet, but I suspect it is something to do with the lack of an SSL certificate.
+
+## Evaluation
+My primary evaluation came in installing a few prerequisites and running the install script. The script is not the most robust for installation, but it attempts to make sure that existing tools are not overwritten.
+
+## Prerequisites
+You will need a Windows 11 machine with the following:
+* WSL installed
+* Powershell 5 running as Administrator
+* `Set-ExecutionPolicy RemoteSigned -Scope CurrentUser` in the admin shell.
+
+## Installation & Usage
+When your environment is setup as described in Prerequisites, just run `startup.ps1` from this repo. 
+
+After this, you *should* be able to follow the tutorial at https://edu.chainguard.dev/open-source/sigstore/policy-controller/how-to-install-policy-controller/#step-4--defining-a-clusterimagepolicy to verify that your Kubernetes installation is respecting your attestation requirements.
+
+Currently, this may not be possible because I am getting an error in the helm install script but this worked earlier in the week.
+
+The output should be something like the following:
+![image of script running](./screen1.png)
--- startup.ps1
+++ startup.ps1
@@ -1,9 +1,127 @@
-﻿Write-Host "[+] Checking for WSL:"
+﻿# Check for Admin
 
+function Update-PathVariable {
+    Write-Host "  - Refreshing PATH after install" -ForegroundColor Yellow
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+
+function Install-Tool {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Toolname,
+        [Parameter(Mandatory=$true)]
+        [string]$Packagename
+    )
+
+    Write-Host "[+] Checking for ${Toolname}:" -ForegroundColor Blue
+    if (Get-Command $Toolname -ErrorAction SilentlyContinue) {
+        Write-Host "  - $Toolname installed!" -ForegroundColor Green
+    } else {
+        Write-Host "  - $Toolname not found, installing via winget:" -ForegroundColor Yellow
+        winget install --id $Packagename -e --silent --accept-source-agreements --accept-package-agreements
+
+        Update-PathVariable
+        if ($Toolname -eq "openssl") {
+            $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + "C:\Program Files\OpenSSL-Win64\bin\"
+        }
+    }
+}
+
+Write-Host "[+] Checking for WSL:" -ForegroundColor Blue
 wsl --status *>&1 > $null
 if ($LASTEXITCODE -eq 0) {
-    Write-Host "  - WSL Installed"
+    Write-Host "  - WSL Installed" -ForegroundColor Green
 } else {
-    Write-Host "  - Installing WSL"
+    Write-Host "  - Installing WSL" -ForegroundColor Yellow
     wsl --install
+
+    Update-PathVariable
+}
+
+Install-Tool "podman" "Redhat.Podman"
+
+Install-Tool "jq" "jqlang.jq"
+
+Install-Tool "openssl" "ShiningLight.OpenSSL.Dev"
+
+Install-Tool "kubectl" "Kubernetes.kubectl"
+
+Install-Tool "minikube" "Kubernetes.minikube"
+
+Install-Tool "helm" "Helm.Helm"
+
+Write-Host "[+] Checking for cosign:" -ForegroundColor Blue
+if (Get-Command cosign -ErrorAction SilentlyContinue) {
+    Write-Host "  - cosign found!" -ForegroundColor Green
+} else {
+    Write-Host "  - cosign not found, installing via winget:" -ForegroundColor Yellow
+    winget install --id Sigstore.Cosign -e --silent --accept-source-agreements --accept-package-agreements
+
+    Update-PathVariable
+
+    # Fix badly named cosign for windows
+    $old_cosign = (get-command cosign-windows-amd64.exe).Path
+    $tmp_cosign = Split-Path -Path $old_cosign
+    New-Item -Path $tmp_cosign\cosign.exe -ItemType SymbolicLink -Value $old_cosign
+}
+
+$env:COSIGN_PASSWORD=""
+cosign generate-key-pair
+
+
+#Write-Host "[+] Creating root CA for Policy Controller"
+#openssl ecparam -genkey -name secp384r1 | openssl ec -out rootCA.key
+#openssl req -x509 -config local.cnf -new -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.crt
+#openssl ecparam -genkey -name prime256v1 -noout -out localhost.key
+#openssl req -new -config ./local.cnf -key localhost.key -out localhost.csr
+#Write-Host "[+] Verifying details for localhost.csr" -ForegroundColor Green
+#openssl req -in localhost.csr -noout -text
+#openssl x509 -req -in localhost.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out localhost.crt -days 397 -sha256 -extfile local.cnf -extensions req_ext
+#Write-Host "[+] Verifying details for localhost.crt" -ForegroundColor Green
+#openssl x509 -in localhost.crt -text -noout
+#openssl x509 -in localhost.crt -noout -ext subjectAltName
+
+Write-Host "[+] Starting podman backend for kubernetes to use:" -ForegroundColor Blue
+try {
+    podman machine init
+    podman machine start
+} catch {
+    Write-Error "Could not start podman, please check installation."
+    exit 1
+}
+
+Write-Host "[+] Starting up minikube rootless environment with podman backend and helm ingress" -ForegroundColor Blue
+try {
+    minikube config set rootless true
+    minikube start --driver=podman --container-runtime=containerd
+    minikube addons enable ingress
+} catch { 
+    Write-Error "Could not start minikube, check installation."
+    exit 1
+}
+
+
+Write-Host "[+] Pre-reqs complete! Setting up Sigstore Policy Controller inside Kubernetes to enforce SLSA compliance"
+try {
+    Write-Host "  - setting up cosign namespace..." -ForegroundColor Green
+    kubectl create namespace cosign-system
+    
+    Write-Host "[+] Adding cosign key to kubernetes" -ForegroundColor Green
+    kubectl create secret generic upstream-public-key --namespace cosign-system --from-file=cosign.pub=cosign.pub
+
+    Write-Host "  - adding cosign repo to helm..." -ForegroundColor Green
+    helm repo add sigstore https://sigstore.github.io/helm-charts
+    helm repo update
+
+    Write-Host "  - installing policy controller..." -ForegroundColor Green
+    helm install policy-controller -n cosign-system sigstore/policy-controller --devel
+
+    WriteHost "  - awaiting cluster for readiness..." -ForegroundColor Green
+    kubectl -n cosign-system wait --for=condition=Available deployment/policy-controller-webhook;
+    kubectl -n cosign-system wait --for=condition=Available deployment/policy-controller-policy-webhook;
+
+    Write-Host "  - enforcing compliance in cluster" -ForegroundColor Green
+    kubectl label namespace default policy.sigstore.dev/include=true
+} catch {
+    Write-Error "Sigstore setup failed. Check error and see if prerequisites exist."
 }
