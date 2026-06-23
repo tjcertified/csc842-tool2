2076176ae6a2b8efd131da81b55e84d316b59e9f
Thomas Williams <tjcertified@gmail.com>
2026-06-22 19:22:03 -0500

    WIP: committing code before renaming

--- startup.ps1
+++ startup.ps1
@@ -1,4 +1,12 @@
 ﻿# Check for Admin
+function Test-AdminPrivileges {
+    $currentUser = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent());
+    if ($currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        return $true
+    } else {
+        return $false
+    }
+}
 
 function Update-PathVariable {
     Write-Host "  - Refreshing PATH after install" -ForegroundColor Yellow
@@ -21,10 +29,41 @@ function Install-Tool {
         winget install --id $Packagename -e --silent --accept-source-agreements --accept-package-agreements
 
         Update-PathVariable
-        if ($Toolname -eq "openssl") {
-            $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") + ";" + "C:\Program Files\OpenSSL-Win64\bin\"
+    }
+}
+
+function Start-RootfulPodman {
+    Write-Host "[+] Check if podman is running..." -ForegroundColor Blue
+    try {
+        # See if podman is currently running
+        $podman_status = $(podman machine inspect --format "{{.State}}")
+        if ($podman_status -eq "running") {
+            Write-Host "  - podman started, stopping to guarantee rootful mode" -ForegroundColor Yellow
+            podman machine stop
+        }
+        Write-Host "  - starting podman in rootful mode for minikube success..." -ForegroundColor Green
+        podman machine set --rootful
+        podman machine start
+    } catch {
+        try {
+            # If it fails, try to init then start
+            podman machine init
+            Write-Host "  - starting podman in rootful mode for minikube success..." -ForegroundColor Green
+            podman machine set --rootful
+            podman machine start
+        } catch {
+            Write-Error "Could not start podman, please check installation and/or errors above."
+            exit 1
         }
     }
+}
+
+Write-Host "[+] Checking for Administrator:" -ForegroundColor Blue
+if (Test-AdminPrivileges) {
+    Write-Host "  - continuing in Administrator session" -ForegroundColor Green
+} else {
+    Write-Host "  - This script requires admin mode. Please open a new PowerShell environment with Administrator privileges." -ForegroundColor Yellow
+    exit 1
 }
 
 Write-Host "[+] Checking for WSL:" -ForegroundColor Blue
@@ -42,7 +81,7 @@ Install-Tool "podman" "Redhat.Podman"
 
 Install-Tool "jq" "jqlang.jq"
 
-Install-Tool "openssl" "ShiningLight.OpenSSL.Dev"
+Install-Tool "mkcert" "FiloSottile.mkcert"
 
 Install-Tool "kubectl" "Kubernetes.kubectl"
 
@@ -65,63 +104,67 @@ if (Get-Command cosign -ErrorAction SilentlyContinue) {
     New-Item -Path $tmp_cosign\cosign.exe -ItemType SymbolicLink -Value $old_cosign
 }
 
-$env:COSIGN_PASSWORD=""
-cosign generate-key-pair
 
-
-#Write-Host "[+] Creating root CA for Policy Controller"
-#openssl ecparam -genkey -name secp384r1 | openssl ec -out rootCA.key
-#openssl req -x509 -config local.cnf -new -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.crt
-#openssl ecparam -genkey -name prime256v1 -noout -out localhost.key
-#openssl req -new -config ./local.cnf -key localhost.key -out localhost.csr
-#Write-Host "[+] Verifying details for localhost.csr" -ForegroundColor Green
-#openssl req -in localhost.csr -noout -text
-#openssl x509 -req -in localhost.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out localhost.crt -days 397 -sha256 -extfile local.cnf -extensions req_ext
-#Write-Host "[+] Verifying details for localhost.crt" -ForegroundColor Green
-#openssl x509 -in localhost.crt -text -noout
-#openssl x509 -in localhost.crt -noout -ext subjectAltName
-
-Write-Host "[+] Starting podman backend for kubernetes to use:" -ForegroundColor Blue
-try {
-    podman machine init
-    podman machine start
-} catch {
-    Write-Error "Could not start podman, please check installation."
-    exit 1
+if (-not (Test-Path -Path "./cosign.key") -and -not (Test-Path -Path "./cosign.pub")) {
+    $env:COSIGN_PASSWORD="1234"
+    $env:COSIGN_YES="true"
+    Write-Host "[+] Generating cosign key-pair for signing artifacts." -ForegroundColor Blue
+    cosign generate-key-pair
 }
 
-Write-Host "[+] Starting up minikube rootless environment with podman backend and helm ingress" -ForegroundColor Blue
+Write-Host "[+] Creating root CA for Policy Controller - NOTE: requires approval, click 'Yes' on prompt if it pops up" -ForegroundColor Blue
+mkcert -install
+New-Item -ItemType Directory -Force -Path ".\certs" | Out-Null
+$env:CAROOT="$($pwd.Path)\certs"
+mkcert.exe -cert-file="$env:CAROOT\tls.crt" -key-file="$env:CAROOT\tls.key" host.minikube.internal 127.0.0.1
+$bytes = [System.IO.File]::ReadAllBytes("$env:CAROOT\rootCA.pem")
+$b64String = [System.Convert]::ToBase64String($bytes)
+
+Start-RootfulPodman
+
+Write-Host "[+] (Re)Starting minikube rootless environment with podman backend and helm ingress" -ForegroundColor Blue
 try {
-    minikube config set rootless true
-    minikube start --driver=podman --container-runtime=containerd
-    minikube addons enable ingress
+    minikube stop
+    # minikube config set rootless true
+    minikube start --driver=podman --container-runtime=containerd --addons=ingress
 } catch { 
     Write-Error "Could not start minikube, check installation."
     exit 1
 }
 
-
-Write-Host "[+] Pre-reqs complete! Setting up Sigstore Policy Controller inside Kubernetes to enforce SLSA compliance"
+Write-Host "[+] Pre-reqs complete! Setting up Kyverno inside Kubernetes to enforce signed containers." -ForegroundColor Blue
 try {
-    Write-Host "  - setting up cosign namespace..." -ForegroundColor Green
-    kubectl create namespace cosign-system
+    #Write-Host "  - setting up cosign namespace..." -ForegroundColor Green
+    #kubectl create namespace cosign-system
+    kubectl create namespace kyverno
     
-    Write-Host "[+] Adding cosign key to kubernetes" -ForegroundColor Green
-    kubectl create secret generic upstream-public-key --namespace cosign-system --from-file=cosign.pub=cosign.pub
+    #Write-Host "  - Adding cosign key to kubernetes" -ForegroundColor Green
+    #kubectl create secret generic upstream-public-key --namespace cosign-system --from-file=cosign.pub=cosign.pub
 
-    Write-Host "  - adding cosign repo to helm..." -ForegroundColor Green
-    helm repo add sigstore https://sigstore.github.io/helm-charts
+    #Write-Host "  - adding cosign repo to helm..." -ForegroundColor Green
+    #helm repo add sigstore https://sigstore.github.io/helm-charts
+    #helm repo update
+    Write-Host "  - adding Kyverno repo to helm..." -ForegroundColor Green
+    helm repo add kyverno https://kyverno.github.io/kyverno/
     helm repo update
 
-    Write-Host "  - installing policy controller..." -ForegroundColor Green
-    helm install policy-controller -n cosign-system sigstore/policy-controller --devel
+    #Write-Host "  - installing policy controller..." -ForegroundColor Green
+    #helm install policy-controller -n cosign-system sigstore/policy-controller --devel
+    Write-Host "  - installing Kyverno..." -ForegroundColor Green
+    helm install kyverno kyverno/kyverno -n kyverno
+    
 
-    WriteHost "  - awaiting cluster for readiness..." -ForegroundColor Green
-    kubectl -n cosign-system wait --for=condition=Available deployment/policy-controller-webhook;
-    kubectl -n cosign-system wait --for=condition=Available deployment/policy-controller-policy-webhook;
+    #Write-Host "  - awaiting cluster for readiness..." -ForegroundColor Green
+    #kubectl -n cosign-system wait --for=condition=Available deployment/policy-controller-webhook;
+    #kubectl -n cosign-system wait --for=condition=Available deployment/policy-controller-policy-webhook;
 
     Write-Host "  - enforcing compliance in cluster" -ForegroundColor Green
     kubectl label namespace default policy.sigstore.dev/include=true
 } catch {
-    Write-Error "Sigstore setup failed. Check error and see if prerequisites exist."
+    Write-Error "Sigstore setup failed. Check error and see if prerequisites exist. Removing environment"
+    minikube delete    
+    exit 1
 }
+
+Write-Host "[+] Starting minikube tunnel for ingress"
+Start-Job -ScriptBlock { minikube tunnel } -Name "TunnelJob"
